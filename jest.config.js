1c2b5563524fdaa9c1477f5beb5138cfc545b8f5
MAC <andresbuendia560@gmail.com>
2024-08-10 03:09:13 -0500

    feat: 🎸 end proyect

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,33 @@
+// module.exports = {
+//   preset: 'jest-preset-angular',
+//   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+//   globals: {
+//     'ts-jest': {
+//       tsconfig: '<rootDir>/tsconfig.spec.json',
+//       stringifyContentPathRegex: '\\.html$',
+//       astTransformers: {
+//         before: ['jest-preset-angular/build/InlineFilesTransformer', 'jest-preset-angular/build/StripStylesTransformer'],
+//       },
+//     },
+//   },
+//   testPathIgnorePatterns: ['/node_modules/', '/dist/', '/src/test.ts'],
+//   transform: {
+//     '^.+\\.(ts|html)$': 'ts-jest',
+//   },
+//   moduleNameMapper: {
+//     '^@app/(.*)$': '<rootDir>/src/app/$1',
+//     '^@environments/(.*)$': '<rootDir>/src/environments/$1',
+//   },
+//   moduleFileExtensions: ['ts', 'html', 'js', 'json'],
+//   coverageDirectory: '<rootDir>/coverage/',
+// };
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.html$',
-      astTransformers: {
-        before: ['jest-preset-angular/build/InlineFilesTransformer', 'jest-preset-angular/build/StripStylesTransformer'],
-      },
-    },
-  },
-  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/src/test.ts'],
   transform: {
-    '^.+\\.(ts|html)$': 'ts-jest',
+    '^.+\\.(ts|js|html)$': 'ts-jest',
   },
-  moduleNameMapper: {
-    '^@app/(.*)$': '<rootDir>/src/app/$1',
-    '^@environments/(.*)$': '<rootDir>/src/environments/$1',
-  },
-  moduleFileExtensions: ['ts', 'html', 'js', 'json'],
-  coverageDirectory: '<rootDir>/coverage/',
+  testMatch: ['**/+(*.)+(spec).+(ts|js)?(x)'],
+  coverageDirectory: './coverage',
+  collectCoverage: true,
 };
